497810a36000fa139798c4c00b96cf1f0ecbe434
Sumanbitw <007sumanghosh@gmail.com>
2021-08-07 22:40:44 +0530

    refactor

--- api/routes/router.history.js
+++ api/routes/router.history.js
@@ -1,42 +1,62 @@
-const express = require("express")
-const history = require("../models/history.model")
-const mongoose = require("mongoose")
-const router = express.Router()
-const Video = require("../models/video.model")
+const express = require("express");
+const history = require("../models/history.model");
+const mongoose = require("mongoose");
+const router = express.Router();
+const Video = require("../models/video.model");
 
-router.get("/:userId", async ( req,res ) => {
-    try {
-        const { userId } = req.params
-        const userVideos = await history.find({ user : {_id : userId } }).populate('video').exec();
-        res.json({ video : userVideos, success : true, message : "Videos fetched" })
-    } catch ( err ) {
-        res.json({ message : err, success : false, message : " Failed to fetch videos" })
-    }  
-})
-
-router.post("/", async (req,res) => {
+router.get("/:userId", async (req, res) => {
   try {
-    const video = new history(req.body)
-    const newVideo = await video.save()
-    res.json({ success : true, message : "History videos created", history : newVideo })
-}catch(error){
-    res.json({ success : false, message : "History videos cannot be created", error : error})
-}
+    const { userId } = req.params;
+    const userVideos = await history
+      .find({ user: { _id: userId } })
+      .populate("video")
+      .exec();
+    res.json({ video: userVideos, success: true, message: "Videos fetched" });
+  } catch (err) {
+    res.json({
+      message: err,
+      success: false,
+      message: " Failed to fetch videos",
+    });
+  }
+});
 
-  })
+router.post("/", async (req, res) => {
+  try {
+    const video = new history(req.body);
+    const newVideo = await video.save();
+    res.json({
+      success: true,
+      message: "History videos created",
+      history: newVideo,
+    });
+  } catch (error) {
+    res.json({
+      success: false,
+      message: "History videos cannot be created",
+      error: error,
+    });
+  }
+});
 
 router.delete("/:userId/:videoId", async (req, res) => {
-    const { userId, videoId } = req.params
-    try {
-        const removedHistoryVideos = await history.remove({ user : userId, videoId : videoId })
-        console.log(removedHistoryVideos)
-        res.json({ success : true, message : "Videos removed", removedBookmarkVideos : removedHistoryVideos })
-    }catch(error){
-        res.json({
-            success : false,
-            message : "Error while removing videos"
-        })
-    }
-})
+  const { userId, videoId } = req.params;
+  try {
+    const removedHistoryVideos = await history.remove({
+      user: userId,
+      videoId: videoId,
+    });
+    res.json({
+      success: true,
+      message: "Videos removed",
+      removedHistoryVideos: removedHistoryVideos,
+    });
+  } catch (error) {
+    res.json({
+      success: false,
+      message: "Error while removing videos",
+    });
+  }
+});
 
-module.exports = router
+module.exports = router;
